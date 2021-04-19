2cd4a67a63c59af527185f2430225f169d12022a
Mick de Graaf <mick@mickdegraaf.nl>
2021-04-19 13:06:45 +0100

    remove early withdraw and deposit module

--- contracts/SharesTimeLock.sol
+++ contracts/SharesTimeLock.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./base/DelegationModule.sol";
 import "./ERC20DividendsOwned.sol";
 import "./libraries/LowGasSafeMath.sol";
 
 
-contract SharesTimeLock is DelegationModule, Ownable() {
+contract SharesTimeLock is Ownable() {
   using LowGasSafeMath for uint256;
   using TransferHelper for address;
+
+  address public immutable depositToken;
 
   ERC20DividendsOwned public immutable dividendsToken;
 
@@ -17,11 +18,11 @@ contract SharesTimeLock is DelegationModule, Ownable() {
 
   uint32 public immutable maxLockDuration;
 
-  /**
-   * @dev Maximum early withdrawal fee expressed as a fraction of 1e18.
-   * This is the fee paid if tokens are withdrawn immediately after being locked.
-   */
-  uint256 public immutable maxEarlyWithdrawalFee;
+  // /**
+  //  * @dev Maximum early withdrawal fee expressed as a fraction of 1e18.
+  //  * This is the fee paid if tokens are withdrawn immediately after being locked.
+  //  */
+  // uint256 public immutable maxEarlyWithdrawalFee;
 
   /**
    * @dev Maximum dividends multiplier for a lock duration of `maxLockDuration`
@@ -41,19 +42,19 @@ contract SharesTimeLock is DelegationModule, Ownable() {
     return locks.length;
   }
 
-  /**
-   * @dev Returns the fee that will be paid on a deposit of `amount` which was deposited
-   * at `lockedAt` with a timelock of `lockDuration`, if it is withdrawn now.
-   *
-   * The fractional fee is calculated as the time remaining in the timelock divided by the
-   * duration of the lock, multiplied by the maximum early withdrawal fee.
-   */
-  function getEarlyWithdrawalFee(uint256 amount, uint32 lockedAt, uint32 lockDuration) public view returns (uint256 fee) {
-    uint32 unlockAt = lockedAt + lockDuration;
-    if (block.timestamp >= unlockAt) return 0;
-    uint32 timeRemaining = unlockAt - uint32(block.timestamp);
-    return amount.mul(timeRemaining).mul(maxEarlyWithdrawalFee) / (uint256(lockDuration) * 1e18);
-  }
+  // /**
+  //  * @dev Returns the fee that will be paid on a deposit of `amount` which was deposited
+  //  * at `lockedAt` with a timelock of `lockDuration`, if it is withdrawn now.
+  //  *
+  //  * The fractional fee is calculated as the time remaining in the timelock divided by the
+  //  * duration of the lock, multiplied by the maximum early withdrawal fee.
+  //  */
+  // function getEarlyWithdrawalFee(uint256 amount, uint32 lockedAt, uint32 lockDuration) public view returns (uint256 fee) {
+  //   uint32 unlockAt = lockedAt + lockDuration;
+  //   if (block.timestamp >= unlockAt) return 0;
+  //   uint32 timeRemaining = unlockAt - uint32(block.timestamp);
+  //   return amount.mul(timeRemaining).mul(maxEarlyWithdrawalFee) / (uint256(lockDuration) * 1e18);
+  // }
 
   /**
    * @dev Returns the dividends multiplier for `duration` expressed as a fraction of 1e18.
@@ -72,24 +73,22 @@ contract SharesTimeLock is DelegationModule, Ownable() {
     ERC20DividendsOwned dividendsToken_,
     uint32 minLockDuration_,
     uint32 maxLockDuration_,
-    uint256 maxDividendsBonusMultiplier_,
-    uint256 maxEarlyWithdrawalFee_
-  ) payable DelegationModule(depositToken_) {
+    uint256 maxDividendsBonusMultiplier_
+  ) payable {
     dividendsToken = dividendsToken_;
+    depositToken = depositToken_;
     require(minLockDuration_ < maxLockDuration_, "min>=max");
     minLockDuration = minLockDuration_;
     maxLockDuration = maxLockDuration_;
     maxDividendsBonusMultiplier = maxDividendsBonusMultiplier_;
-    require(maxEarlyWithdrawalFee_ <= 1e18, "maxFee");
-    maxEarlyWithdrawalFee = maxEarlyWithdrawalFee_;
   }
 
-  function withdrawFees(address to) external onlyOwner {
-    depositToken.safeTransfer(to, IERC20(depositToken).balanceOf(address(this)));
-  }
+  // function withdrawFees(address to) external onlyOwner {
+  //   depositToken.safeTransfer(to, IERC20(depositToken).balanceOf(address(this)));
+  // }
 
   function deposit(uint256 amount, uint32 duration) external {
-    _depositToModule(msg.sender, amount);
+    depositToken.safeTransferFrom(msg.sender, address(this), amount);
     uint256 multiplier = getDividendsMultiplier(duration);
     uint256 dividendShares = amount.mul(multiplier) / 1e18;
     dividendsToken.mint(msg.sender, dividendShares);
@@ -101,24 +100,16 @@ contract SharesTimeLock is DelegationModule, Ownable() {
     }));
   }
 
-  function delegate(address delegatee) external {
-    _delegateFromModule(msg.sender, delegatee);
-  }
-
   function withdraw(uint256 lockId) external {
     Lock memory lock = locks[lockId];
     require(msg.sender == lock.owner, "!owner");
+    require(block.timestamp > lock.lockedAt + lock.lockDuration, "lock not expired");
     delete locks[lockId];
     uint256 multiplier = getDividendsMultiplier(lock.lockDuration);
     uint256 dividendShares = lock.amount.mul(multiplier) / 1e18;
     dividendsToken.burn(msg.sender, dividendShares);
-    uint256 earlyWithdrawalFee = getEarlyWithdrawalFee(lock.amount, lock.lockedAt, lock.lockDuration);
-    if (earlyWithdrawalFee > 0) {
-      _withdrawFromModule(msg.sender, address(this), lock.amount);
-      depositToken.safeTransfer(msg.sender, lock.amount.sub(earlyWithdrawalFee));
-    } else {
-      _withdrawFromModule(msg.sender, msg.sender, lock.amount);
-    }
+      
+    depositToken.safeTransfer(msg.sender, lock.amount);
   }
 
   // Eject expired locks
@@ -135,7 +126,7 @@ contract SharesTimeLock is DelegationModule, Ownable() {
       uint256 dividendShares = lock.amount.mul(multiplier) / 1e18;
       dividendsToken.burn(lock.owner, dividendShares);
 
-      _withdrawFromModule(lock.owner, address(this), lock.amount);
+      depositToken.safeTransfer(lock.owner, lock.amount);
     }
   }
 }
