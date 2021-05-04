b4efcdddec9d98955b478d3d998dde93ba124a6d
Mick de Graaf <mick@mickdegraaf.nl>
2021-05-04 16:00:28 +0200

    Delete IERC20Delegatable.sol

--- contracts/interfaces/IERC20Delegatable.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity =0.7.6;
-
-import "./IERC20Metadata.sol";
-
-
-interface IERC20Delegatable is IERC20Metadata {
-  function delegate(address delegatee) external;
-}
