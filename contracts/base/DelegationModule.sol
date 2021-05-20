bf8088b748c753aa6307ae9479db2824e8d0da02
d1ll0n <dillonkellar@protonmail.com>
2021-05-19 23:54:27 -0500

    Use deployed contracts instead of self-destructing ones for delegation module

--- contracts/base/DelegationModule.sol
+++ contracts/base/DelegationModule.sol
@@ -4,42 +4,35 @@ pragma solidity ^0.7.0;
 /* ---  External Libraries  --- */
 import { Create2 } from "@openzeppelin/contracts/utils/Create2.sol";
 import "../libraries/TransferHelper.sol";
-import "./SubDelegationModule.sol";
+import "../libraries/CloneLibrary.sol";
+import "./SubDelegationModuleImplementation.sol";
+import "../interfaces/IDelegationModule.sol";
 
 
-contract DelegationModule {
+contract DelegationModule is IDelegationModule {
   using TransferHelper for address;
+  address public immutable override moduleImplementation;
+  address public immutable override depositToken;
 
-  // @todo Fix hardhat error running out of memory with keccak256(type(SubDelegationModule).creationCode)
-
-  bytes32 internal constant INIT_CODE_HASH = keccak256(type(SubDelegationModule).creationCode);
-
-  address public immutable depositToken;
-  address private delegationTarget;
-  bool private delegateOrTransfer;
-  uint256 private transferAmount;
+  /**
+   * @dev Contains the address of the sub-delegation module for a user
+   * if one has been deployed.
+   */
+  mapping(address => ISubDelegationModule) public override subDelegationModuleForUser;
 
   constructor(address depositToken_) {
     depositToken = depositToken_;
+    moduleImplementation = address(new SubDelegationModuleImplementation(depositToken_));
   }
 
-  function getNextAction() external view returns (address, bool, address, uint256) {
-    return (depositToken, delegateOrTransfer, delegationTarget, transferAmount);
-  }
-
-  function computeSubDelegationAddress(address account) public view returns (address) {
-    bytes32 salt = keccak256(abi.encodePacked(account));
-    return Create2.computeAddress(salt, INIT_CODE_HASH);
-  }
-
-  /**
-   * @dev Execute a call to the sub-delegation module for `account`.
-   */
-  function _executeModuleAction(address account) internal {
-    Create2.deploy(0, keccak256(abi.encodePacked(account)), type(SubDelegationModule).creationCode);
-    delegationTarget = address(0);
-    delegateOrTransfer = false;
-    transferAmount = 0;
+  function getOrCreateModule(address account) internal returns (ISubDelegationModule module) {
+    module = subDelegationModuleForUser[account];
+    if (address(module) == address(0)) {
+      module = ISubDelegationModule(CloneLibrary.createClone(moduleImplementation));
+      subDelegationModuleForUser[account] = module;
+      module.delegate(account);
+      emit SubDelegationModuleCreated(account, address(module));
+    }
   }
 
   /**
@@ -47,8 +40,8 @@ contract DelegationModule {
    * module for `account`. 
    */
   function _depositToModule(address account, uint256 amount) internal {
-    address module = computeSubDelegationAddress(account);
-    depositToken.safeTransferFrom(account, module, amount);
+    ISubDelegationModule module = getOrCreateModule(account);
+    depositToken.safeTransferFrom(account, address(module), amount);
   }
 
   /**
@@ -56,10 +49,8 @@ contract DelegationModule {
    * sub-delegation module for `account` to `to`.
    */
   function _withdrawFromModule(address account, address to, uint256 amount) internal {
-    transferAmount = amount;
-    delegationTarget = to;
-    delegateOrTransfer = false;
-    _executeModuleAction(account);
+    ISubDelegationModule module = subDelegationModuleForUser[account];
+    module.transfer(to, amount);
   }
 
   /**
@@ -67,8 +58,7 @@ contract DelegationModule {
    * to `delegatee`.
    */
   function _delegateFromModule(address account, address delegatee) internal {
-    delegationTarget = delegatee;
-    delegateOrTransfer = true;
-    _executeModuleAction(account);
+    ISubDelegationModule module = subDelegationModuleForUser[account];
+    module.delegate(delegatee);
   }
 }
---
+++ contracts/base/SubDelegationModuleImplementation.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "../interfaces/IERC20Delegatable.sol";
+
+
+interface IDelegateController {
+  function getNextAction() external view returns (IERC20Delegatable token, bool delegateOrTransfer, address to, uint256 amount);
+}
+
+
+/**
+ * @dev This is a work-around for the delegation mechanic in COMP.
+ * It allows the balance of a staked governance token to be held in separate wallets
+ * for each user who makes a deposit so that they will retain the ability to control
+ * their governance delegation individually.
+ *
+ * This is an implementation contract that should be used for a separate proxy per user.
+ */
+contract SubDelegationModuleImplementation {
+  IERC20Delegatable public immutable token;
+  address public immutable module;
+
+
+  constructor(address _token) {
+    token = IERC20Delegatable(_token);
+    module = msg.sender;
+  }
+
+  function delegate(address to) external {
+    require(msg.sender == module, "!module");
+    token.delegate(to);
+  }
+
+  function transfer(address to, uint256 amount) external {
+    require(msg.sender == module, "!module");
+    token.transfer(to, amount);
+  }
+}
