09431ddcb089dd7d910fffd4e01da6a4e6b3b02f
d1ll0n <dillonkellar@protonmail.com>
2021-09-17 14:12:49 -0500

    Use WETH as payment token and allow ETH withdrawal/distribution

--- contracts/ERC20DividendsOwned.sol
+++ contracts/ERC20DividendsOwned.sol
@@ -4,19 +4,22 @@ pragma solidity =0.7.6;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./base/ERC20Dividends.sol";
 import "./libraries/TransferHelper.sol";
+import "./interfaces/IWETH.sol";
 
 
 contract ERC20DividendsOwned is ERC20Dividends, Ownable {
   using TransferHelper for address;
 
-  address public immutable token;
+  address public immutable weth;
+
+  receive() external payable { return; }
 
   constructor(
-    address token_,
+    address weth_,
     string memory name_,
     string memory symbol_
   ) ERC20Dividends(name_, symbol_) Ownable() {
-    token = token_;
+    weth = weth_;
   }
 
   function mint(address to, uint256 amount) external onlyOwner {
@@ -27,17 +30,24 @@ contract ERC20DividendsOwned is ERC20Dividends, Ownable {
     _burn(from, amount);
   }
 
-  function collectFor(address account) public {
-    uint256 amount = _prepareCollect(account);
-    token.safeTransfer(account, amount);
+  function collect() external {
+    uint256 amount = _prepareCollect(msg.sender);
+    weth.safeTransfer(msg.sender, amount);
   }
 
-  function collect() external {
-    collectFor(msg.sender);
+  function collectETH() external {
+    uint256 amount = _prepareCollect(msg.sender);
+    IWETH(weth).withdraw(amount);
+    address(msg.sender).safeTransferETH(amount);
   }
 
   function distribute(uint256 amount) external {
-    token.safeTransferFrom(msg.sender, address(this), amount);
+    weth.safeTransferFrom(msg.sender, address(this), amount);
     _distributeDividends(amount);
+  }
+
+  function distribute() external payable {
+    IWETH(weth).deposit{value: msg.value}();
+    _distributeDividends(msg.value);
   }
 }
--- contracts/test/TestERC20.sol
+++ contracts/test/TestERC20.sol
@@ -1,33 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.7.6;
 
-import "../base/ERC20.sol";
+import "../base/ERC20VotesComp.sol";
+import "../libraries/TransferHelper.sol";
 
 
-contract TestERC20 is ERC20 {
-  mapping(address => address) public delegates;
+contract TestERC20 is ERC20VotesComp {
+  using TransferHelper for address;
+
   event Delegate(address indexed delegator, address indexed delegatee);
 
-  constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_, 18) {}
+  constructor(string memory name_, string memory symbol_) ERC20VotesComp(name_, symbol_) {}
 
   function mint(address account, uint256 amount) external {
     _mint(account, amount);
-  }
-
-  function delegate(address delegatee) external {
-    delegates[msg.sender] = delegatee;
-    emit Delegate(msg.sender, delegatee);
   }
 
   function burn(address account, uint256 amount) public {
     _burn(account, amount);
   }
 
-  function transferInternal(address from, address to, uint256 value) public {
+  function transferInternal(address from, address to, uint96 value) public {
     _transfer(from, to, value);
   }
 
-  function approveInternal(address owner, address spender, uint256 value) public {
-    _approve(owner, spender, value);
+  function deposit() external payable {
+    _mint(msg.sender, msg.value);
+  }
+
+  function withdraw(uint256 amount) external {
+    _burn(msg.sender, amount);
+    address(msg.sender).safeTransferETH(amount);
   }
 }
